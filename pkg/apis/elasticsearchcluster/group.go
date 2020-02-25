3520c719e11ee0e10a5dd5b28b0ce3d437f10e2f
Prakash Mishra <prakashmishra1598@gmail.com>
2020-02-25 21:54:54 +0530

    Add operator API
    Signed-off-by: Prakash Mishra <prakashmishra1598@gmail.com>

---
+++ pkg/apis/elasticsearchcluster/group.go
@@ -0,0 +1,6 @@
+// Package elasticsearchcluster contains elasticsearchcluster API versions.
+//
+// This file ensures Go source parsers acknowledge the elasticsearchcluster package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package elasticsearchcluster
